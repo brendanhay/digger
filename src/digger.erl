669bde24a730a98dca245635692a81f5247be8a6
brendanhay <brendan.g.hay@gmail.com>
2012-03-29 14:55:59 +0200

    Preliminary template parsing

--- src/digger.erl
+++ src/digger.erl
@@ -34,13 +34,14 @@
 %% API
 -export([main/1]).
 
+-include_lib("amqp_client/include/amqp_client.hrl").
+
 %%
 %% API
 %%
 
-main([]) -> io:fwrite(usage());
-
-main(Configs) -> ok.
+main([])       -> io:fwrite(usage());
+main(_Configs) -> io:fwrite("~s", [render()]).
 
 %%
 %% Private
@@ -48,3 +49,53 @@ main(Configs) -> ok.
 
 usage() ->
     io_lib:fwrite("Usage: ~s CONFIG_FILE~n", [?MODULE]).
+
+render() ->
+    Sources = lookup(sources, test()),
+    {ok, D} = config_dtl:render([
+        {shovels, [shovel(S, test()) || S <- Sources]}
+    ]),
+    D.
+
+shovel(Source, Ctx) ->
+    Destination = lookup(destination, Ctx),
+    Queue = queue_name(Source, Ctx),
+    [{source, escape(Source)},
+     {destination, escape(Destination)},
+     {declarations, [
+         exchange_declaration(Ctx)
+     ]},
+     {queue, escape(Queue)}].
+
+shovel_name(Source, Ctx) ->
+    {ok, #amqp_params_network{host = Host}} = amqp_uri:parse(Source),
+    string:join([exchange_name(Ctx), Host], ".").
+
+queue_name(Source, Ctx) -> shovel_name(Source, Ctx) ++ ".shovel".
+
+exchange_name(Ctx) -> lookup(name, lookup(exchange, Ctx)).
+
+%% Use partial views for each of the declaration types and format specifically
+exchange_declaration(Ctx) ->
+    Args = lists:keydelete(name, 1, lookup(exchange, Ctx)),
+    {'exchange.declare', [
+        {exchange, list_to_binary(exchange_name(Ctx))}
+    ] ++ Args}.
+
+test() ->
+    [{sources, [
+         "amqp://guest:guest@hostfoo",
+         "amqp://guest:guest@hostbar"
+     ]},
+     {destination, "amqp://guest:guest@baz.int"},
+     {exchange, [
+         {name, "exchangename"},
+         {type, <<"direct">>},
+         durable
+     ]}].
+
+lookup(Key, List) ->
+    {Key, Value} = lists:keyfind(Key, 1, List),
+    Value.
+
+escape(Str) -> lists:concat(["\"", Str, "\""]).
