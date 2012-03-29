3e0f46f9fdb8f5b642e5a812c9dfe9b83cd9afac
brendanhay <brendan.g.hay@gmail.com>
2012-03-29 18:24:29 +0200

    Switching to mustache templates

--- src/digger.erl
+++ src/digger.erl
@@ -34,10 +34,6 @@
 %% API
 -export([main/1]).
 
--include_lib("amqp_client/include/amqp_client.hrl").
-
--compile(export_all).
-
 %%
 %% API
 %%
@@ -45,8 +41,10 @@
 main([]) ->
     main(["bin/digger.conf"]);
 main([Path]) ->
-    {ok, Config} = file:consult(Path),
-    io:fwrite("~s", [render(Config)]).
+    {ok, [Config]} = file:consult(Path),
+    Ctx = context(Config),
+    Result = mustache:render(digger_view, template(digger_view), Ctx),
+    io:fwrite("~p~n", [Result]).
 
 %%
 %% Private
@@ -55,61 +53,10 @@ main([Path]) ->
 usage() ->
     io_lib:fwrite("Usage: ~s CONFIG_FILE~n", [?MODULE]).
 
-render([Config]) ->
-    {ok, D} = config_dtl:render([
-        {shovels, lists:concat([shovels(C) || C <- Config])}
-    ]),
-    D.
+context(Config) ->
+    dict:from_list([{shovels, [dict:from_list(C) || C <- Config]}])
 
-shovels(Ctx) -> [shovel(S, Ctx) || S <- lookup(sources, Ctx)].
-
-shovel(Source, Ctx) ->
-    Destination = lookup(destination, Ctx),
-    Queue = queue_name(Source, Ctx),
-    [{name, shovel_name(Source, Ctx)},
-     {source, escape(Source)},
-     {destination, escape(Destination)},
-     {declarations, [
-         exchange_declaration(Ctx),
-         queue_declaration(Source, Ctx)
-         | binding_declarations(Source, Ctx)
-     ]},
-     {queue, "<<" ++ escape(Queue) ++ ">>"}].
-
-shovel_name(Source, Ctx) ->
-    {ok, #amqp_params_network{host = Host}} = amqp_uri:parse(Source),
-    string:join([exchange_name(Ctx), Host], ".").
-
-queue_name(Source, Ctx) -> shovel_name(Source, Ctx) ++ ".shovel".
-
-queue_declaration(Source, Ctx) ->
-    {'queue.declare', [
-        {queue, bin(queue_name(Source, Ctx))},
-        durable
-    ]}.
-
-exchange_name(Ctx) -> lookup(name, lookup(exchange, Ctx)).
-
-%% Use partial views for each of the declaration types and format specifically
-exchange_declaration(Ctx) ->
-    {'exchange.declare', [
-        {exchange, bin(exchange_name(Ctx))}
-        | lists:keydelete(name, 1, lookup(exchange, Ctx))
-    ]}.
-
-binding_declarations(Source, Ctx) ->
-    [{'queue.bind', [
-        {exchange, bin(exchange_name(Ctx))},
-        {queue, bin(queue_name(Source, Ctx))},
-        {routing_key, bin(K)}
-    ]} || K <- lookup(routing_keys, Ctx)].
-
-lookup(Key, List) ->
-    {Key, Value} = lists:keyfind(Key, 1, List),
-    Value.
-
-escape(Str) -> lists:concat(["\"", Str, "\""]).
-
-bin(List) when is_list(List)  -> list_to_binary(List);
-bin(Atom) when is_atom(Atom)  -> atom_to_binary(Atom, latin1);
-bin(Bin)  when is_binary(Bin) -> Bin.
+template(Mod) ->
+    {ok, Cwd} = file:get_cwd(),
+    File = atom_to_list(Mod) ++ ".mustache",
+    string:join([Cwd, "templates", File], "/").
