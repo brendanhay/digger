6228b86cfd82fce96724c9fa144d01b65ff8de77
brendanhay <brendan.g.hay@gmail.com>
2012-03-30 12:11:27 +0200

    Removing comments

--- src/digger.erl
+++ src/digger.erl
@@ -8,35 +8,12 @@
 %% @doc
 %%
 
-%% Arguments:
-%%   Source:
-%%     Amqp Urls
-%%     Exchange (+ details)
-%%     Routing Key
-
-%%   Destination:
-%%     Amqp Url
-%%     Exchange (+ details)
-
-%%   Prefetch
-%%   Reconnect Delay in seconds
-
-%% Actions:
-%%   Create a named shovel for each unique source amqp url
-%%   Declare the intermediary shovel queue with a sensible name
-%%   Ack Mode should be on_confirm
-
-%% Questions:
-%%   Durability, persistent messages ala federation queues
-
 -module(digger).
 
 %% API
 -export([main/1]).
 
 -include_lib("amqp_client/include/amqp_client.hrl").
-
--compile(export_all).
 
 %%
 %% API
